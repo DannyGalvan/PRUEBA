0f77b1063e5eff13c4214df8dc0c57fc73b614bb
DannyGalvan <cgalvan29111999@gmail.com>
2021-09-22 09:10:32 -0600

    empezamos con la interfas

--- indesjs.js
+++ indesjs.js
@@ -1,6 +1,18 @@
 var nombre = prompt("cual es tu nombre");
+parrafo.innerHTML += "usuario " + nombre + "</br>";
+parrafo.innerHTML += "el usuario que esta entrando es: " + nombre + "</br>";
 
-parrafo.innerHTML += "HOLA MUNDO" + "</br>"
-parrafo.innerHTML += nombre;
+var respuesta = prompt("como va tu dia " + nombre);
+parrafo.innerHTML += "consola: como va tu dia" + "</br>";
+parrafo.innerHTML += nombre + " : " + respuesta + "</br>";
+
+var edad = prompt("quiero saber tu edad " + nombre + " me la podrias dar");
+parrafo.innerHTML += "consola: quiero saber tu edad " + nombre + "</br>";
+parrafo.innerHTML += "esta es tu edad " + nombre + " : " + edad + "</br>";
+
+
+
+
+
 
 
