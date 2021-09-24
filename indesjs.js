a43f84f4544ea21e9c96873eeaa83b2fa28b1731
DannyGalvan <cgalvan29111999@gmail.com>
2021-09-24 07:12:23 -0600

    cambiando una linea de codigo

--- indesjs.js
+++ indesjs.js
@@ -7,8 +7,8 @@ var nombre = prompt("cual es tu nombre");
 parrafo.innerHTML += "usuario " + nombre + "</br>";
 parrafo.innerHTML += "el usuario que esta entrando es: " + nombre + "</br>";
 
-var respuesta = prompt("como va tu dia " + nombre);
-parrafo.innerHTML += "consola: como va tu dia" + "</br>";
+var respuesta = prompt("puedo saber donde vives? " + nombre);
+parrafo.innerHTML += "consola: puedo saber donde vives?" + "</br>";
 parrafo.innerHTML += nombre + " : " + respuesta + "</br>";
 
 var edad = prompt("quiero saber tu edad " + nombre + " me la podrias dar");
